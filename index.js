c604cbf7198771a97302447539df075b9c4bbfa4
oluwatobi balogun <tobibags19@gmail.com>
2019-07-15 14:27:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(`${selector}`);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+  console.log(deepestChild().innerHTML)
+}
+
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll('.ranked-list')
+  
+  for (let i = 0; i < ranks.length; i++) {
+    let children = ranks[i].children; {
+      for(let j = 0; j < children.length; j++) {
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+      }
+    }
+  
+  }
+}
